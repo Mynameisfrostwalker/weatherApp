da9de7262fe8780a822e100bdef3033ba12160a1
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-08-26 22:23:41 +0100

    created pubsub functions

---
+++ src/scripts/pubsub.js
@@ -0,0 +1,51 @@
+// @ts-check
+
+/**
+ * @module pubsub
+ */
+
+/**
+ * @type {Object} - Object of events.
+ */
+const events = {};
+
+/**
+ * Subscribe to an event
+ * @param {string} eventName - Name of the event
+ * @param {Function} fn - Function to be called
+ */
+const subscribe = function (eventName, fn) {
+  events[eventName] = events[eventName] || [];
+  events[eventName].push(fn);
+};
+
+/**
+ * Unsuscribe from an event
+ * @param {string} eventName - Name of the event
+ * @param {Function} fn - Function to be removed
+ */
+const unsubscribe = function (eventName, fn) {
+  if (events[eventName]) {
+    for (let i = 0; i < events[eventName].length; i++) {
+      if (events[eventName][i] === fn) {
+        events[eventName].splice(i, 1);
+        break;
+      }
+    }
+  }
+};
+
+/**
+ * Publish an event
+ * @param {string} eventName - Name of the event
+ * @param {*} data -data to be passed into callback
+ */
+const publish = function (eventName, data) {
+  if (events[eventName]) {
+    events[eventName].forEach(function (fn) {
+      fn(data);
+    });
+  }
+};
+
+export { publish, unsubscribe, subscribe };
